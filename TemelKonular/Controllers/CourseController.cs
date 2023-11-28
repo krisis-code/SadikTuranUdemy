70391b5f6325a7ecdfa2d005608de13d9060ea38
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2023-11-28 21:09:06 +0300

    Ders 15 bitti

--- TemelKonular/Controllers/CourseController.cs
+++ TemelKonular/Controllers/CourseController.cs
@@ -19,9 +19,11 @@ public IActionResult List()
 {
     var kurslar = new List<Course>
     {
-        new Course (){Id = 1, Title ="Aspnet Core Kursu 1" , Description="A dan Z ye tüm detayları ile"},
-        new Course (){Id = 2, Title ="Aspnet Core Kursu 2" , Description="A dan Z ye tüm detayları ile"},
-        new Course (){Id = 3, Title ="Aspnet Core Kursu 3" , Description="A dan Z ye tüm detayları ile"},
+        new Course (){Id = 1, Title ="Komple Uygulamalı Web Geliştirme Eğitimi" ,
+         Description="Sıfırdan ileri seviyeye 'Web Geliştirme': Html, Css, Sass, Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc&Core Mvc" , image = "1.jpg"},
+        new Course (){Id = 2, Title ="Sıfırdan İleri Seviye Modern Javascript Dersleri ES7+" , Description="Modern javascript dersleri ile (ES6 & ES7+) Nodejs, Angular, React ve VueJs için sağlam bir temel oluşturun.",image="2.jpg"},
+        new Course (){Id = 3, Title ="Node.js ile Sıfırdan İleri Seviye Web Geliştirme" , Description="Node.js ile sıfırdan ileri seviye dinamik web uygulaması geliştirmeyi öğren.",image="3.jpg"},
+        new Course (){Id = 4, Title ="Python ile Sıfırdan İleri Seviye Python Programlama" , Description="Sıfırdan İleri Seviye Python Dersleri.Veritabanı,Veri Analizi,Bot Yazımı,Web Geliştirme(Django).",image="4.jpg"},
     };
     return View("CourseList",kurslar);
 }
--- TemelKonular/Models/Course.cs
+++ TemelKonular/Models/Course.cs
@@ -5,6 +5,6 @@ namespace TemelKonular.Models;
         public int Id { get; set; }
         public string? Title { get; set; }
         
-        public staring? image { get; set; }
+        public string? image { get; set; }
         public string? Description { get; set; }
     }
