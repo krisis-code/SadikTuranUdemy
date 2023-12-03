2ca086febf11897bc9659a4872aad9857625c6c7
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2023-12-03 20:02:42 +0300

    Ders 20 bitti

--- TemelKonular/Controllers/CourseController.cs
+++ TemelKonular/Controllers/CourseController.cs
@@ -8,13 +8,25 @@ namespace TemelKonular.Controllers;
 public class CourseController : Controller{
 public IActionResult Index()
 {
-    var kurs = new Course();
-    kurs.Id = 1;
-    kurs.Title="Komple Uygulamalı Web Geliştirme Eğitimi";
-    kurs.Description = "Sıfırdan ileri seviyeye 'Web Geliştirme': Html, Css, Sass, Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc&Core Mvc";
-    kurs.image="1.jpg";
-    
-    return View(kurs);
+               var kurs = new Course();
+            
+                kurs.Id = 1;
+                kurs.Title = "Komple Uygulamalı Web Geliştirme Eğitimi";
+                kurs.Description = "Sıfırdan ileri seviyeye 'Web Geliştirme': Html, Css, Sass, Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc&Core Mvc";
+                kurs.image = "1.jpg";
+            
+            return View(kurs);
+}
+public IActionResult Details()
+{
+               var kurs = new Course();
+            
+                kurs.Id = 1;
+                kurs.Title = "Komple Uygulamalı Web Geliştirme Eğitimi";
+                kurs.Description = "Sıfırdan ileri seviyeye 'Web Geliştirme': Html, Css, Sass, Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc&Core Mvc";
+                kurs.image = "1.jpg";
+            
+            return View(kurs);
 }
 public IActionResult List()
 {
