17b602415f394cf2b3757accaa9f64a54ac4e877
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2023-12-03 20:41:17 +0300

    Ders 21 bitti

--- TemelKonular/Controllers/CourseController.cs
+++ TemelKonular/Controllers/CourseController.cs
@@ -6,17 +6,17 @@ using TemelKonular.Models;
 namespace TemelKonular.Controllers;
 
 public class CourseController : Controller{
-public IActionResult Index()
-{
-               var kurs = new Course();
-            
-                kurs.Id = 1;
-                kurs.Title = "Komple Uygulamalı Web Geliştirme Eğitimi";
-                kurs.Description = "Sıfırdan ileri seviyeye 'Web Geliştirme': Html, Css, Sass, Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc&Core Mvc";
-                kurs.image = "1.jpg";
-            
-            return View(kurs);
-}
+    public IActionResult Index()
+    {
+        var kurs = new Course();
+           
+        kurs.Id = 1;
+        kurs.Title = "Komple Uygulamalı Web Geliştirme Eğitimi";
+        kurs.Description = "Sıfırdan ileri seviyeye 'Web Geliştirme': Html, Css, Sass, Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc&Core Mvc";
+        kurs.image = "1.jpg";
+
+        return View(kurs);
+    }
 public IActionResult Details()
 {
                var kurs = new Course();
@@ -30,15 +30,8 @@ public IActionResult Details()
 }
 public IActionResult List()
 {
-    var kurslar = new List<Course>
-    {
-        new Course (){Id = 1, Title ="Komple Uygulamalı Web Geliştirme Eğitimi" ,
-         Description="Sıfırdan ileri seviyeye 'Web Geliştirme': Html, Css, Sass, Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc&Core Mvc" , image = "1.jpg"},
-        new Course (){Id = 2, Title ="Sıfırdan İleri Seviye Modern Javascript Dersleri ES7+" , Description="Modern javascript dersleri ile (ES6 & ES7+) Nodejs, Angular, React ve VueJs için sağlam bir temel oluşturun.",image="2.jpg"},
-        new Course (){Id = 3, Title ="Node.js ile Sıfırdan İleri Seviye Web Geliştirme" , Description="Node.js ile sıfırdan ileri seviye dinamik web uygulaması geliştirmeyi öğren.",image="3.jpg"},
-        new Course (){Id = 4, Title ="Python ile Sıfırdan İleri Seviye Python Programlama" , Description="Sıfırdan İleri Seviye Python Dersleri.Veritabanı,Veri Analizi,Bot Yazımı,Web Geliştirme(Django).",image="4.jpg"},
-    };
-    return View("CourseList",kurslar);
+    
+    return View("CourseList",Repository.Courses);
 }
 
 }
