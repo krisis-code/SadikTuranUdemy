f840aa9fe655143492c6b467b8c5e92cac49bce4
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2023-12-17 22:08:37 +0300

    Ders 57 bitti

--- FormsApp/Controllers/HomeController.cs
+++ FormsApp/Controllers/HomeController.cs
@@ -7,34 +7,32 @@ namespace FormsApp.Controllers;
 
 public class HomeController : Controller
 {
-   
-
-   
-
-    public IActionResult Index(string searchString , string Categories)
+    public HomeController()
     {
-        var product = Repository.Products;
-        
-        if(! string.IsNullOrEmpty(searchString))
+    }
+
+    [HttpGet]
+    public IActionResult Index(string searchString, string category)
+    {
+        var products = Repository.Products;
+
+        if(!String.IsNullOrEmpty(searchString))
         {
             ViewBag.SearchString = searchString;
-            product = product.Where(p=> p.Name.ToLower().Contains(searchString)).ToList();
-
+            products = products.Where(p => p.Name.ToLower().Contains(searchString)).ToList();
         }
-         
-        if(! string.IsNullOrEmpty(Categories) && Categories != "0")
+
+        if(!String.IsNullOrEmpty(category) && category != "0")
         {
-           
-            product = product.Where(p=> p.CategoryId==int.Parse(Categories)).ToList();
-
+            products = products.Where(p => p.CategoryId == int.Parse(category)).ToList();
         }
-        // ViewBag.Categories = new SelectList(Repository.Categories,"CategoryId","Name",Categories);
 
-        var model = new ProductViewModel{
-        Products=product,
-        Categories=Repository.Categories,
-        SelectedCategory=Categories
+        // ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name", category);
 
+        var model = new ProductViewModel {
+            Products = products,
+            Categories = Repository.Categories,
+            SelectedCategory = category
         };
 
         return View(model);
@@ -43,47 +41,50 @@ public class HomeController : Controller
     [HttpGet]
     public IActionResult Create()
     {
-        ViewBag.Categories = new SelectList(Repository.Categories,"CategoryId","Name");
+        ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name");
         return View();
     }
+
+    
     [HttpPost]
-    public async Task <IActionResult> Create(Product model, IFormFile imageFile)
+    public async Task<IActionResult> Create(Product model, IFormFile imageFile)
     {
         var extension = "";
-        if(imageFile != null)
-        {
-            var allowedExtension = new [] {".jpg",".jpeg",".png"};
-             extension = Path.GetExtension(imageFile.FileName);
-            if(!allowedExtension.Contains(extension))
+        if(imageFile != null) {
+            var allowedExtensions = new[] {".jpg",".jpeg",".png"};
+            extension = Path.GetExtension(imageFile.FileName); // abc.jpg
+
+            if(!allowedExtensions.Contains(extension)) 
             {
-            ModelState.AddModelError("","Geçerli bir format seçiniz(.jpg , .jpeg , .png)");
+                ModelState.AddModelError("", "Geçerli bir resim seçiniz.");
             }
         }
+
         if(ModelState.IsValid)
         {
-            if(imageFile != null)
-            {  
-            var randomFileName = string.Format($"{Guid.NewGuid().ToString()}{extension}");
-            var path = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/img",randomFileName);
-            using(var stream = new FileStream(path,FileMode.Create))
+            if(imageFile != null) 
             {
-                await imageFile.CopyToAsync(stream);
-
+                var randomFileName = string.Format($"{Guid.NewGuid().ToString()}{extension}");
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img", randomFileName);
+                using(var stream = new FileStream(path, FileMode.Create))
+                {
+                    await imageFile.CopyToAsync(stream);
+                }
+                model.Image = randomFileName;
+                model.ProductId = Repository.Products.Count + 1;
+                Repository.CreateProduct(model);
+                return RedirectToAction("Index");
             }
-             model.Image=randomFileName;
-            model.ProductId = Repository.Products.Count + 1;
-            Repository.CreateProduct(model);
-            return RedirectToAction("Index");
-            }
-           
+          
         }
-        ViewBag.Categories = new SelectList(Repository.Categories,"CategoryId","Name");
+        ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name");
         return View(model);
-        
+
     }
+
     public IActionResult Edit(int? id)
     {
-        if(id == null)
+        if(id == null) 
         {
             return NotFound();
         }
@@ -92,55 +93,81 @@ public class HomeController : Controller
         {
             return NotFound();
         }
-        ViewBag.Categories = new SelectList(Repository.Categories,"CategoryId","Name");
+        ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name");
         return View(entity);
     }
 
     [HttpPost]
- public async Task<IActionResult> Edit(int id, Product model, IFormFile? imageFile)
- {
-     if(id != model.ProductId)
-     {
-         return NotFound();
-     }
+    public async Task<IActionResult> Edit(int id, Product model, IFormFile? imageFile)
+    {
+        if(id != model.ProductId)
+        {
+            return NotFound();
+        }
 
-     if(ModelState.IsValid)
-     {
-         if(imageFile != null) 
-         {
-             var extension = Path.GetExtension(imageFile.FileName); // abc.jpg
-             var randomFileName = string.Format($"{Guid.NewGuid().ToString()}{extension}");
-             var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img", randomFileName);
+        if(ModelState.IsValid)
+        {
+            if(imageFile != null) 
+            {
+                var extension = Path.GetExtension(imageFile.FileName); // abc.jpg
+                var randomFileName = string.Format($"{Guid.NewGuid().ToString()}{extension}");
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img", randomFileName);
 
-             using(var stream = new FileStream(path, FileMode.Create))
-             {
-                 await imageFile.CopyToAsync(stream);
-             }
-             model.Image = randomFileName;
-         }
-         Repository.EditProduct(model);
-         return RedirectToAction("Index");
-     }
-     ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name");
-     return View(model);
- }
+                using(var stream = new FileStream(path, FileMode.Create))
+                {
+                    await imageFile.CopyToAsync(stream);
+                }
+                model.Image = randomFileName;
+            }
+            Repository.EditProduct(model);
+            return RedirectToAction("Index");
+        }
+        ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name");
+        return View(model);
+    }
 
-     public IActionResult Delete(int? id)
- {
-     if(id == null)
-     {
-         return NotFound();        
-     }
 
-     var entity = Repository.Products.FirstOrDefault(p => p.ProductId == id);
-     if(entity == null)
-     {
-         return NotFound();
-     }
-    Repository.DeleteProduct(entity);
-     return View("Index");
- }
+    public IActionResult Delete(int? id)
+    {
+        if(id == null)
+        {
+            return NotFound();        
+        }
 
- 
-   
+        var entity = Repository.Products.FirstOrDefault(p => p.ProductId == id);
+        if(entity == null)
+        {
+            return NotFound();
+        }
+
+        return View("DeleteConfirm", entity);
+    }
+
+    [HttpPost]
+    public IActionResult Delete(int id, int ProductId)
+    {
+        if(id != ProductId)
+        {
+            return NotFound();
+        }
+
+        var entity = Repository.Products.FirstOrDefault(p => p.ProductId == ProductId);
+        if(entity == null)
+        {
+            return NotFound();
+        }
+
+        Repository.DeleteProduct(entity);
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public IActionResult EditProducts(List<Product> Products)
+    {
+        foreach (var product in Products)
+        {
+            Repository.EditIsActive(product);
+        }
+        return RedirectToAction("Index");
+    }
 }
--- FormsApp/Models/Repository.cs
+++ FormsApp/Models/Repository.cs
@@ -1,75 +1,79 @@
-namespace FormsApp.Models;
-
-public class Repository
+namespace FormsApp.Models
 {
-   private static readonly List<Product> _products = new();
-   private static readonly List<Category> _categories = new();
-
-    static Repository()
+    public class Repository
     {
-         _categories.Add(new Category{CategoryId = 0 , Name = "Hepsi"});
-        _categories.Add(new Category{CategoryId = 1 , Name = "Telefon"});
-        _categories.Add(new Category{CategoryId = 2 , Name = "Bilgisayar"});
+        private static readonly List<Product> _products = new();
+        private static readonly List<Category> _categories = new();
+        static Repository()
+        {
+            _categories.Add(new Category { CategoryId = 1, Name = "Telefon" });
+            _categories.Add(new Category { CategoryId = 2, Name = "Bilgisayar" });
 
-         _products.Add(new Product{ProductId = 1 , Name = "iPhone 13  satın alın",Price= 40000, IsActive = true ,Image = "1.jpg" , CategoryId =1});
+            _products.Add(new Product {ProductId = 1, Name = "IPhone 14", Price = 40000, IsActive = true,Image = "1.jpg",CategoryId = 1 });
+            _products.Add(new Product {ProductId = 2, Name = "IPhone 15", Price = 50000, IsActive = false,Image = "2.jpg",CategoryId = 1 });
+            _products.Add(new Product {ProductId = 3, Name = "IPhone 16", Price = 60000, IsActive = true,Image = "3.jpg",CategoryId = 1 });
+            _products.Add(new Product {ProductId = 4, Name = "IPhone 17", Price = 70000, IsActive = false,Image = "4.jpg",CategoryId = 1 });
+            
+            _products.Add(new Product {ProductId = 5, Name = "Macbook Air", Price = 80000, IsActive = false,Image = "5.jpg",CategoryId = 2 });
+            _products.Add(new Product {ProductId = 6, Name = "Macbook Pro", Price = 90000, IsActive = true,Image = "6.jpg",CategoryId = 2 });
+        }
 
-         _products.Add(new Product{ProductId = 2 , Name = "iPhone 13  satın alın",Price= 40000, IsActive = true ,Image = "2.jpg",CategoryId =1});
+        public static List<Product> Products
+        {
+            get
+            {
+                return _products;
+            }
+        }
 
-         _products.Add(new Product{ProductId = 3 , Name = "iPhone 15 Pro satın alın",Price= 60000, IsActive = true ,Image = "3.jpg",CategoryId=1});
+        public static void CreateProduct(Product entity)
+        {
+            _products.Add(entity);
+        }
 
-         _products.Add(new Product{ProductId = 4 , Name = "iPhone 15 Pro satın alın",Price= 60000, IsActive = false ,Image = "4.jpg",CategoryId=1});
+        public static void EditProduct(Product updatedProduct)
+        {
+            var entity = _products.FirstOrDefault(p => p.ProductId == updatedProduct.ProductId);
 
-         _products.Add(new Product{ProductId = 5 , Name = "iPhone 15 Pro satın alın",Price= 60000, IsActive = true ,Image = "5.jpg",CategoryId=1});
+            if(entity != null) 
+            {
+                if(!string.IsNullOrEmpty(updatedProduct.Name)) 
+                {
+                    entity.Name = updatedProduct.Name;
+                }
+                entity.Price = updatedProduct.Price;
+                entity.Image = updatedProduct.Image;
+                entity.CategoryId = updatedProduct.CategoryId;
+                entity.IsActive = updatedProduct.IsActive;
+            }
+        }
 
-         _products.Add(new Product{ProductId = 6 , Name = "iPhone 15 Pro Max satın alın",Price= 80000, IsActive = false ,Image = "6.jpg",CategoryId =1});
+        public static void EditIsActive(Product updatedProduct)
+        {
+            var entity = _products.FirstOrDefault(p => p.ProductId == updatedProduct.ProductId);
 
-         _products.Add(new Product{ProductId = 7 , Name = "iPhone 15 Pro Max satın alın",Price= 80000, IsActive = true ,Image = "7.jpg",CategoryId=1});
+            if(entity != null) 
+            {
+                entity.IsActive = updatedProduct.IsActive;
+            }
+        }
 
-         _products.Add(new Product{ProductId = 8 , Name = "14 inç MacBook Pro’nuzu",Price= 80000, IsActive = true ,Image = "8.jpg",CategoryId=2});
+        public static void DeleteProduct(Product deletedProduct)
+        {
+            var entity = _products.FirstOrDefault(p => p.ProductId == deletedProduct.ProductId);
 
-        _products.Add(new Product{ProductId = 9 , Name = "14 inç MacBook Air",Price= 50000, IsActive = true ,Image = "9.jpg",CategoryId=2});
-        
+            if(entity != null) 
+            {
+                _products.Remove(entity);
+            }
+        }
+
+        public static List<Category> Categories
+        {
+            get
+            {
+                return _categories;
+            }
+        }
     }
-   public static  List<Product> Products 
-   {
-    get{
-        return _products;
-    }
-
-   }
-
-   public static void CreateProduct(Product entity)
-   {
-    _products.Add(entity);
-   }
-
-   public static void EditProduct(Product updatedProduct)
-   {
-    var entity = _products.FirstOrDefault(p => p.ProductId == updatedProduct.ProductId);
-    if(entity != null)
-    {
-        entity.Name = updatedProduct.Name;
-        entity.Price = updatedProduct.Price;
-        entity.Image = updatedProduct.Image;
-        entity.CategoryId = updatedProduct.CategoryId;
-    }
-   }
-    public static void DeleteProduct(Product deletedProduct)
- {
-     var entity = _products.FirstOrDefault(p => p.ProductId == deletedProduct.ProductId);
-
-     if(entity != null) 
-     {
-         _products.Remove(entity);
-     }
- }
-
-   public static List<Category> Categories
-   {
-    get{
-        return _categories;
-    }
-   }
-
-
 }
