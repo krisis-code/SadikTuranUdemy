14c9135beffcf5c858481338574937ae4c7826d0
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2023-12-19 21:49:13 +0300

    Ders 62 bitti datacontext oluşturuldu

---
+++ efcoreApp/Data/DataContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace efcoreApp.Data
+{
+
+    public class DataContext : DbContext
+    {
+        public DbSet<Kurs> Kurslar => Set<Kurs>();
+
+        public DbSet<Ogrenci> Ogrenciler => Set<Ogrenci>();
+
+        public DbSet<KursKayit> Kurskayitleri => Set<KursKayit>();
+
+    }
+
+}
