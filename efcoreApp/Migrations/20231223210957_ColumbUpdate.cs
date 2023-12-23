c5c560ac0c3b052410866c96ce4739e14280f363
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2023-12-24 00:33:07 +0300

    ders 82 bitti bu proje bitti

---
+++ efcoreApp/Migrations/20231223210957_ColumbUpdate.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace efcoreApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class ColumbUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Kurslar_Ogretmenler_OgretmenId",
+                table: "Kurslar");
+
+            migrationBuilder.DropColumn(
+                name: "OgrentmenId",
+                table: "Kurslar");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OgretmenId",
+                table: "Kurslar",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Kurslar_Ogretmenler_OgretmenId",
+                table: "Kurslar",
+                column: "OgretmenId",
+                principalTable: "Ogretmenler",
+                principalColumn: "OgretmenId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Kurslar_Ogretmenler_OgretmenId",
+                table: "Kurslar");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OgretmenId",
+                table: "Kurslar",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddColumn<int>(
+                name: "OgrentmenId",
+                table: "Kurslar",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Kurslar_Ogretmenler_OgretmenId",
+                table: "Kurslar",
+                column: "OgretmenId",
+                principalTable: "Ogretmenler",
+                principalColumn: "OgretmenId");
+        }
+    }
+}
