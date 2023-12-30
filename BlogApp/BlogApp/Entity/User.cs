0322519477734d4932f1d069864f0d28a2219ae8
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2023-12-30 18:12:37 +0300

    Ders 102 bitti yorum listesi eklendi

--- BlogApp/BlogApp/Entity/User.cs
+++ BlogApp/BlogApp/Entity/User.cs
@@ -6,6 +6,8 @@
 
         public string? UserName { get; set; }
 
+        public string? Image { get; set; }
+
         public List<Post> Posts { get; set; } = new List<Post>();
 
         public List<Comment> Comments { get; set; } = new List<Comment>();
