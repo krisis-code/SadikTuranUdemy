7b7f08a91bbfb2692b32bd195317d6a5d1d25d6d
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2024-01-02 21:47:45 +0300

    Ders 107 bitti Login sayfası ve login view model eklendi

---
+++ BlogApp/BlogApp/Models/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using BlogApp.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogApp.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name ="Eposta")]
+        public string? Email { get; set; }
+
+        [Required]
+        [StringLength(maximumLength:20 , MinimumLength = 6 , ErrorMessage = "Min 6 maks 20 karakter belirtiniz")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Şifre")]
+        public string? Password { get; set; }
+
+
+
+    }
+}
