bae6d83484899a3b6b07bf76e45374cd2a25532b
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2024-01-10 19:47:41 +0300

    Ders 123 bitti user list view yapıldı

---
+++ IdentityApp/IdentityApp/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityApp.Controllers
+{
+    public class UsersController : Controller
+    {
+        private UserManager<IdentityUser> _userManager;
+        public UsersController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        
+        public IActionResult Index()
+        {
+            return View(_userManager.Users);
+        }
+    }
+}
