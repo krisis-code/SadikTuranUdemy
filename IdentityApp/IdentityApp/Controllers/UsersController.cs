a3aba878c91bdfaed46223bc3c038c58e7f00c6c
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2024-01-12 23:50:14 +0300

    Ders 124 bitti kullanıcı ekleme işlemi yapıldı

--- IdentityApp/IdentityApp/Controllers/UsersController.cs
+++ IdentityApp/IdentityApp/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using IdentityApp.ViewModel;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityApp.Controllers
@@ -14,6 +15,33 @@ namespace IdentityApp.Controllers
         public IActionResult Index()
         {
             return View(_userManager.Users);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = model.UserName, Email = model.Email };
+
+             IdentityResult result =  await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                foreach (IdentityError  err in result.Errors)
+                {
+                    ModelState.AddModelError("", err.Description);
+                }
+            
+            }
+            return View(model);
         }
     }
 }
---
+++ IdentityApp/IdentityApp/ViewModel/CreateViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityApp.ViewModel
+{
+    public class CreateViewModel
+    {
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage ="Parola Eşleşmiyor")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+
+    }
+}
