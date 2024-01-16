b274a75bc101fe96f095128d99f7777ea6d0695c
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2024-01-16 21:02:28 +0300

    Ders 130 bitti Yeni role ekleme ekranı eklendi

---
+++ IdentityApp/IdentityApp/Controllers/RolesController.cs
@@ -0,0 +1,44 @@
+﻿using IdentityApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityApp.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+        public RolesController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(AppRole model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var result = await _roleManager.CreateAsync(model);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                foreach (var err in result.Errors)
+                {
+                    ModelState.AddModelError("", err.Description);
+                }
+            }
+            return View(model);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
