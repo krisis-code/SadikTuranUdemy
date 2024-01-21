83af63930186be6c37623a9655cec72de4674d35
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2024-01-21 23:31:49 +0300

    RoleUserTagHelpers clasının içi dolduruldu

--- IdentityApp/IdentityApp/TagHelpers/RoleUsersTagHelper.cs
+++ IdentityApp/IdentityApp/TagHelpers/RoleUsersTagHelper.cs
@@ -1,7 +1,11 @@
-﻿namespace IdentityApp.TagHelpers
-{
-    public class RoleUsersTagHelper
-    {
-
-    }
-}
+﻿using IdentityApp.Models;
+using Microsoft.AspNetCore.Identity;using Microsoft.AspNetCore.Razor.TagHelpers;namespace IdentityApp.TagHelpers{    [HtmlTargetElement("td" , Attributes = "asp-role-users")]    public class RoleUsersTagHelper : TagHelper    {        private readonly RoleManager<AppRole> _roleManager;        private readonly UserManager<AppUser> _userManager;        public RoleUsersTagHelper(RoleManager<AppRole> roleManager , UserManager<AppUser> userManager)        {            _roleManager = roleManager;            _userManager = userManager;        }        [HtmlAttributeName("asp-role-users")]        public string RoleId { get; set; } = null!;        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)        {           var userNames = new List<string>();            var role = await _roleManager.FindByIdAsync(RoleId);            if (role != null && role.Name != null)
+            {
+                foreach (var user in _userManager.Users)
+                {
+                    if (await _userManager.IsInRoleAsync(user,role.Name))
+                    {
+                        userNames.Add(user.UserName ?? "");
+                    }
+                }
+            }        }    }}
