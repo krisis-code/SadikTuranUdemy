b0002035403fa8841d99a537fc713a1e00cff458
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2024-01-22 22:00:27 +0300

    createviewmodel e Usernames prop eklendi ve create.cshyml username alanı eklendi

--- IdentityApp/IdentityApp/ViewModel/CreateViewModel.cs
+++ IdentityApp/IdentityApp/ViewModel/CreateViewModel.cs
@@ -8,6 +8,10 @@ namespace IdentityApp.ViewModel
         [Required]
         public string FullName { get; set; } = string.Empty;
 
+
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+
         [Required]
         [EmailAddress]
         public string Email { get; set; } = string.Empty;
