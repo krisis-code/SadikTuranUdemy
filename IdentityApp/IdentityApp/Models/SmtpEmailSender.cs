6ee78020b2044bc8a5d3e7b5b398aee14992589c
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2024-01-27 15:18:46 +0300

    SmtpEmailSender class iç.i dolduruldu

--- IdentityApp/IdentityApp/Models/SmtpEmailSender.cs
+++ IdentityApp/IdentityApp/Models/SmtpEmailSender.cs
@@ -1,4 +1,7 @@
-﻿namespace IdentityApp.Models
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace IdentityApp.Models
 {
     public class SmtpEmailSender : IEmailSender
     {
@@ -6,19 +9,30 @@
 
         private int _port;
 
-        private bool enableSSL;
+        private bool _enableSSL;
 
         private string _username;
 
         private string _password;
 
-        public SmtpEmailSender(string host , int port,)
+        public SmtpEmailSender(string host , int port, bool enablessl, string username , string password)
         {
-            
+            _host = host;
+            _port = port;
+            _enableSSL = enablessl;
+            _username = username;
+            _password = password;
         }
         public Task SendEmailAsync(string email, string subject, string message)
         {
-            throw new NotImplementedException();
+            var client = new SmtpClient(_host, _port)
+            {
+                Credentials = new NetworkCredential(_username, _password),
+                EnableSsl = _enableSSL
+
+            };
+
+           return client.SendMailAsync(new MailMessage(_username , email, subject, message) { IsBodyHtml = true});
         }
     }
 }
