28922b548b3de0c874e4466ba0c74f0a3e3256bb
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2024-01-27 15:14:27 +0300

    emailsender ve smtpsender clasları eklendi

---
+++ IdentityApp/IdentityApp/Models/IEmailSender.cs
@@ -0,0 +1,8 @@
+﻿namespace IdentityApp.Models
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email ,  string subject , string message );
+
+    }
+}
---
+++ IdentityApp/IdentityApp/Models/SmtpEmailSender.cs
@@ -0,0 +1,24 @@
+﻿namespace IdentityApp.Models
+{
+    public class SmtpEmailSender : IEmailSender
+    {
+        private string _host;
+
+        private int _port;
+
+        private bool enableSSL;
+
+        private string _username;
+
+        private string _password;
+
+        public SmtpEmailSender(string host , int port,)
+        {
+            
+        }
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
