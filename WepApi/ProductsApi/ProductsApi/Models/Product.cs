c00927c0cf9df22079703433306def00bd2a58d3
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2024-01-29 22:46:28 +0300

    Product modeli eklendi ve prop lar eklendi

---
+++ WepApi/ProductsApi/ProductsApi/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace ProductsApi.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public String ProductName { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
