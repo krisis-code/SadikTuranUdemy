b75ebf5b8d08223d07be1bb6eb1ed7ae8af0880b
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2024-01-29 22:07:37 +0300

    ProductsController eklendi

---
+++ WepApi/ProductsApi/ProductsApi/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductsApi.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
