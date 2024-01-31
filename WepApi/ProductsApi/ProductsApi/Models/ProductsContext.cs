533e4b04c442a471f0532ff9ed75c6f42278e324
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2024-01-31 22:46:21 +0300

    data context eklendi

---
+++ WepApi/ProductsApi/ProductsApi/Models/ProductsContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProductsApi.Models
+{
+    public class ProductsContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public ProductsContext(DbContextOptions<ProductsContext> options) : base(options)
+        {
+            
+        }
+    }
+}
