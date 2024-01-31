44a4d859b0c594ce8bd0071d0c3b2799134ec04b
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2024-01-31 23:20:43 +0300

    veri tabanı eklenince listeleme işlemleri güncellendi.

--- WepApi/ProductsApi/ProductsApi/Controllers/ProductsController.cs
+++ WepApi/ProductsApi/ProductsApi/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ProductsApi.Models;
 
 namespace ProductsApi.Controllers
@@ -7,38 +8,34 @@ namespace ProductsApi.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        public static List<Product>? _products;
-
-        public ProductsController()
+        public readonly ProductsContext _context;
+        public ProductsController(ProductsContext context)
         {
-            _products = new List<Product>
-            {
-                 new() { ProductId = 0, ProductName = "IPhone 12", Price = 50000, IsActive = true },
-                 new()  { ProductId = 1, ProductName = "IPhone 13", Price = 60000, IsActive = true },
-                 new()  { ProductId = 2, ProductName = "IPhone 14", Price = 70000, IsActive = true },
-                 new()  { ProductId = 3, ProductName = "IPhone 15", Price = 80000, IsActive = true }
-            };
+           _context = context;
         }
            
         [HttpGet]
-        public IActionResult GetProducts()
+        public async Task<IActionResult> GetProducts()
         {
-            if (_products == null)
+            var products = await _context.Products.ToListAsync();   
+
+            if (products == null)
             {
                 return NotFound();
             }
-            return Ok(_products );
+            return Ok(products);
         }
 
         [HttpGet("id")]
-        public IActionResult GetProduct(int? id)
+        public async Task<IActionResult> GetProduct(int? id)
         {
+
             if (id == null)
             {
                return NotFound();
             }
 
-            var product = _products.FirstOrDefault(i => i.ProductId == id);
+            var product = await _context.Products.FirstOrDefaultAsync( i => i.ProductId == id);
 
             if (product == null)
             {
