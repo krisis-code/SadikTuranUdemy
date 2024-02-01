3b96ac61feac9ac516189aea4ca89616112fc699
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2024-02-01 22:47:37 +0300

    DTO klasörü ve clası eklendi dto clası proplar eklendi

---
+++ WepApi/ProductsApi/ProductsApi/DTO/ProductDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductsApi.DTO
+{
+    public class ProductDTO
+    {
+        public int ProductId { get; set; }
+
+        public String ProductName { get; set; } = null!;
+
+        public decimal Price { get; set; }
+    }
+}
