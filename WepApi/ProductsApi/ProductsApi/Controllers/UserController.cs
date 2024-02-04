53901b8afd0463f61ff31aa556a10b08688a2808
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2024-02-04 17:14:33 +0300

    Token keyu güncellendi

--- WepApi/ProductsApi/ProductsApi/Controllers/UserController.cs
+++ WepApi/ProductsApi/ProductsApi/Controllers/UserController.cs
@@ -11,60 +11,62 @@ namespace ProductsApi.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class UserController : ControllerBase
+    public class UsersController : ControllerBase
     {
         private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _singInManager;
+        private readonly IConfiguration _configuration;
 
-        private readonly SignInManager<AppUser> _signInManager;
-
-        private readonly IConfiguration _configuraiton;
-
-        public UserController(UserManager<AppUser> userManager , SignInManager<AppUser> signInManager , IConfiguration configuraiton)
+        public UsersController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, IConfiguration configuration)
         {
             _userManager = userManager;
-            _signInManager = signInManager;
-            _configuraiton = configuraiton;
+            _singInManager = signInManager;
+            _configuration = configuration;
         }
 
         [HttpPost("register")]
-
         public async Task<IActionResult> CreateUser(userDTO model)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
+
             var user = new AppUser
             {
+                FullName = model.FullName,
                 UserName = model.UserName,
                 Email = model.Email,
-                FullName = model.FullName,
-                AddedTime = DateTime.Now, 
-
+                AddedTime = DateTime.Now
             };
 
-            var result = await _userManager.CreateAsync(user , model.Password);
+            var result = await _userManager.CreateAsync(user, model.Password);
 
             if (result.Succeeded)
             {
                 return StatusCode(201);
             }
-            return Ok();
-           
+            return
+            BadRequest(result.Errors);
         }
 
-        public async Task<IActionResult> Login (LoginDTO model)
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDTO model)
         {
             var user = await _userManager.FindByEmailAsync(model.Email);
 
-            if (user == null) {
-                return BadRequest(new { message = "Mail hatalı" });
+            if (user == null)
+            {
+                return BadRequest(new { message = "email hatalı" });
             }
-            var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
+
+            var result = await _singInManager.CheckPasswordSignInAsync(user, model.Password, false);
 
             if (result.Succeeded)
             {
-                return Ok(new { token = GenerateJWT(user) });
+                return Ok(
+                    new { token = GenerateJWT(user) }
+                );
             }
             return Unauthorized();
         }
@@ -72,18 +74,15 @@ namespace ProductsApi.Controllers
         private object GenerateJWT(AppUser user)
         {
             var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.ASCII.GetBytes(_configuraiton.GetSection("AppSettings:Secret").Value ?? "");
+            var key = Encoding.ASCII.GetBytes(_configuration.GetSection("AppSettings:Secret").Value ?? "");
             var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(
-                    new Claim[]
-                    {
+                    new Claim[] {
                         new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
                         new Claim(ClaimTypes.Name, user.UserName ?? ""),
-
                     }
-
-                    ),
+                ),
                 Expires = DateTime.UtcNow.AddDays(1),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
             };
