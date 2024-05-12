cb878e676a3b7d5f28c34a61e7d7fe888f4b8be7
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-05-12 11:30:39 +0300

    mvc için gerekli servisler ekelndi

--- StoreApp/StoreApp.Web/Program.cs
+++ StoreApp/StoreApp.Web/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+app.UseStaticFiles();
+app.MapDefaultControllerRoute();
 
 app.Run();
