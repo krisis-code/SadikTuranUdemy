f9ee512951a734440835915d8424bb5008e1f5d1
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-05-15 22:34:03 +0300

    interface implamente edildi ve metod yazıldı

--- StoreApp/StoreApp.Data/Concrete/EFStoreRepository.cs
+++ StoreApp/StoreApp.Data/Concrete/EFStoreRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StoreApp.data.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,19 @@ using System.Threading.Tasks;
 
 namespace StoreApp.data.Concrete
 {
-	public class EFStoreRepository
+	public class EFStoreRepository : IStoreRepository
 	{
+		private StoreDbContext _context;
+
+        public EFStoreRepository(StoreDbContext context)
+        {
+            _context = context;
+        }
+		public IQueryable<Product> Products => _context.Products;
+
+		public void CreateProduct(Product entity)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
