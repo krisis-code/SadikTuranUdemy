dfbe901eb53911a207774677f5542e79f5f5c093
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-05-19 12:41:38 +0300

    ProductViewModel eklendi ve içeriği dolduruldu

---
+++ StoreApp/StoreApp.Web/Models/ProductViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace StoreApp.web.Models
+{
+	public class ProductViewModel
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = string.Empty;
+		public string Description { get; set; } = string.Empty;
+		public decimal Price { get; set; }
+		public string Category { get; set; } = string.Empty;
+	}
+}
