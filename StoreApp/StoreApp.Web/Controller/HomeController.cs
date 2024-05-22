79866e34af8069b923f17832a710ecbe6fef84c6
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-05-22 23:31:07 +0300

    Pagination işlemi yapıldı

--- StoreApp/StoreApp.Web/Controller/HomeController.cs
+++ StoreApp/StoreApp.Web/Controller/HomeController.cs
@@ -7,21 +7,22 @@ namespace StoreApp.web.Controllers
     public class HomeController : Controller
     {
         private IStoreRepository _repository;
+		public int pageSize = 3;
 
 		public HomeController(IStoreRepository repository)
 		{
 			_repository = repository;
 		}
 
-		public IActionResult Index() {
+		public IActionResult Index(int page = 1) {
 
-			var products = _repository.Products.Select(p => new ProductViewModel
+			var products = _repository.Products.Skip((page-1)*pageSize).Select(p => new ProductViewModel
 			{
 				Id = p.Id,
 				Name = p.Name,
 				Description = p.Description,
 				Price = p.Price
-			}).ToList(); 
+			}).Take(pageSize); 
 			
 			return View(new ProductListViewModel { Products = products});
 		}
